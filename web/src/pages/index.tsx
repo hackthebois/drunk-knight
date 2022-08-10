c82ecef8e0d10aa1b1743e1be5f5637428da5c8c
Billy Hawkes <70971818+billyhawkes@users.noreply.github.com>
2022-08-09 20:57:46 -0400

    Added database connection for cards, new styling

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,34 +1,43 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 import useReq from "../hooks/useCard";
-import { FaInfoCircle } from "react-icons/fa";
+import { FaGlassWhiskey, FaInfoCircle } from "react-icons/fa";
 import { useRouter } from "next/router";
 import { Card } from "../types/Card";
 
 const typeColours = {
-	CATEGORIES: "449e46",
+	CATEGORIES: "#449e46",
 	ACTION: "#FFA500",
 	MEMORY: "#0DA2FF",
 	MAJORITY: "#937DC2",
 };
 
-const CardItem = ({ id, name, description, card_type }: Card) => {
+const CardItem = ({ name, description, card_type }: Card) => {
 	return (
-		<div
-			className="p-8 rounded m-4 w-[90%] max-w-[400px] shadow"
-			style={{ backgroundColor: typeColours[card_type] }}
-		>
-			<h3 className="text-white text-3xl font-bold text-center">{name}</h3>
-			<p className="text-white text-lg mt-4 text-center">{description}</p>
+		<div className="max-w-[400px] min-w-[300px]">
+			<div
+				className="rounded-t w-[100%] shadow h-24 relative flex items-start justify-between p-4"
+				style={{ backgroundColor: typeColours[card_type] }}
+			>
+				<div className="bg-white flex p-2 items-center rounded text-text font-bold h-10">
+					{card_type}
+				</div>
+				<div className="bg-white flex p-2 items-center rounded text-text h-10">
+					<p className="font-bold mr-1 text-lg">2</p> <FaGlassWhiskey />
+				</div>
+			</div>
+			<div className="bg-white p-8 rounded-b w-[100%]">
+				<h3 className="text-text text-2xl font-bold text-left">{name}</h3>
+				<p className="text-text text-lg mt-4 text-left">{description}</p>
+			</div>
 		</div>
 	);
 };
 
-type Props = {
-	cards: Card[];
-};
-
-const CardStack = ({ cards }: Props) => {
+const Home: NextPage = () => {
+	const { getCard } = useReq();
+	const router = useRouter();
+	const { data: cards } = getCard;
 	const [cardNum, setCardNum] = useState(0);
 
 	const nextCard = () => {
@@ -37,23 +46,6 @@ const CardStack = ({ cards }: Props) => {
 	};
 
 	return (
-		<>
-			{cards && <CardItem {...cards[cardNum]} />}
-			<button
-				onClick={nextCard}
-				className="bg-white font-bold px-4 py-2 mt-4 rounded text-lg shadow"
-			>
-				Next Card
-			</button>
-		</>
-	);
-};
-
-const Home: NextPage = () => {
-	const { getCard } = useReq();
-	const router = useRouter();
-	const { data: cards } = getCard;
-	return (
 		<div className="bg-background h-[100vh] w-[100vw] overflow-hidden flex justify-center items-center flex-col pb-32">
 			<div
 				className="absolute top-0 right-0 flex justify-center items-center p-4"
@@ -61,7 +53,13 @@ const Home: NextPage = () => {
 			>
 				<FaInfoCircle fill="#ffffff" className="w-6 h-6" />
 			</div>
-			{cards && <CardStack cards={cards} />}
+			{cards && <CardItem {...cards[cardNum]} />}
+			<button
+				onClick={nextCard}
+				className="bg-white font-bold px-4 py-2 mt-4 rounded text-lg shadow"
+			>
+				Next Card
+			</button>
 		</div>
 	);
 };
