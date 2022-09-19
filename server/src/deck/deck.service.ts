e91981bc09146c0c9c9f145a4b2e6be15f140a02
filipslepcev <filipslepcev@yahoo.ca>
2022-09-19 14:32:40 -0400

    fixed id bug

--- server/src/deck/deck.service.ts
+++ server/src/deck/deck.service.ts
@@ -1,106 +1,107 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateDeckDto, DeckResponseDto, UpdateDeckDto } from './dto/deck.dto';
 
 @Injectable()
 export class DeckService {
+  constructor(private readonly prismaService: PrismaService) {}
 
-    constructor(private readonly prismaService: PrismaService){}
+  async getAllDecks(userId: string) {
+    const user = await this.prismaService.user.findFirst({
+      where: {
+        id: userId,
+      },
+      select: {
+        decks: true,
+      },
+    });
 
-    async getAllDecks(userId: string){
-        const user = await this.prismaService.user.findFirst({
-            where: {
-                id: userId
-            },
-            select: {
-                decks: true
-            }
-                
-        });
+    if (!user) throw new NotFoundException();
 
-        if (!user) throw new NotFoundException();
+    return user.decks.map((card) => new DeckResponseDto(card));
+  }
 
-        return user.decks.map(card => new DeckResponseDto(card));
-    }
+  async getDeckById(id: string) {
+    const deck = await this.prismaService.deck.findFirst({
+      where: {
+        id: id,
+      },
+      select: {
+        id: true,
+        name: true,
+        selected: true,
+        cards: true,
+      },
+    });
 
-    async getDeckById(id: string) {
-        const deck = await this.prismaService.deck.findFirst({
-            where: {
-                id: id
-            },
-            select: {
-                name: true,
-                selected: true,
-                cards: true
-            }
-                
-        });
+    if (!deck) throw new NotFoundException();
 
-        if (!deck) throw new NotFoundException();
+    return deck;
+  }
 
-        return deck;
-    }
+  async createDeck(userId: string, { name }: CreateDeckDto) {
+    const cardExists = await this.prismaService.card.findFirst({
+      where: {
+        name,
+      },
+    });
 
-    async createDeck(userId: string, { name }: CreateDeckDto) {
-        const cardExists = await this.prismaService.card.findFirst({
-            where:{
-                name
-            }
-        });
-        
-        if(cardExists) throw new ConflictException();
+    if (cardExists) throw new ConflictException();
 
-        const card = await this.prismaService.deck.create({
-            data: {
-                name: name,
-                selected: false,
-                user_id: userId
-            }
-        });
+    const card = await this.prismaService.deck.create({
+      data: {
+        name: name,
+        selected: false,
+        user_id: userId,
+      },
+    });
 
-        return new DeckResponseDto(card);
-    }
+    return new DeckResponseDto(card);
+  }
 
-    async updateDeckById(id: string, data: UpdateDeckDto) {
-        const updatedCard = await this.prismaService.deck.update({
-            where:{
-                id
-            },
-            data,
-        });
+  async updateDeckById(id: string, data: UpdateDeckDto) {
+    const updatedCard = await this.prismaService.deck.update({
+      where: {
+        id,
+      },
+      data,
+    });
 
-        return new DeckResponseDto(updatedCard);
-    }
+    return new DeckResponseDto(updatedCard);
+  }
 
-    async deleteDeckById(id: string) {
-        await this.prismaService.deck.delete({
-            where:{
-                id,
-            }
-        });
+  async deleteDeckById(id: string) {
+    await this.prismaService.deck.delete({
+      where: {
+        id,
+      },
+    });
 
-        return "Deck Successfully Deleted"
-    }
+    return 'Deck Successfully Deleted';
+  }
 
-    async getUserByDeckId(id: string){
-        
-        const deck = await this.prismaService.deck.findUnique({
-            where: {
-                id
-            },
-            select: {
-                user: {
-                    select: {
-                        id: true,
-                        username: true,
-                        email: true,
-                    }
-                }
-            }
-        });
+  async getUserByDeckId(id: string) {
+    const deck = await this.prismaService.deck.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        user: {
+          select: {
+            id: true,
+            username: true,
+            email: true,
+          },
+        },
+      },
+    });
 
-        if(!deck) throw new NotFoundException()
+    if (!deck) throw new NotFoundException();
 
-        return deck.user;
-    }
+    return deck.user;
+  }
 }
