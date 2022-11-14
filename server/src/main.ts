351165e0349c1bc2e212458445c74c884d44dea6
filipslepcev <filipslepcev@yahoo.ca>
2022-11-13 21:36:28 -0500

    added reponse time to swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -21,7 +21,11 @@ async function bootstrap() {
 		.setVersion('1.0')
 		.build();
 	const document = SwaggerModule.createDocument(app, config);
-  	SwaggerModule.setup('api', app, document);
+	SwaggerModule.setup('api', app, document, {
+		swaggerOptions: {
+			displayRequestDuration: true,
+		},
+	});
 
 	app.enableCors();
 	await app.listen(8000);
