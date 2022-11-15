8f54a58e1fa7884817126be40619aeddf7a484fc
billyhawkes <billyhawkes02@gmail.com>
2022-11-15 15:57:54 -0500

    Added optimistic updates to standard deck

--- web/app/decks/GuestDeck.tsx
+++ web/app/decks/GuestDeck.tsx
@@ -1,38 +1,60 @@
 "use client";
 
+import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { FaPause, FaPlay } from "react-icons/fa";
 
 type Props = {
 	useStandard: boolean;
 };
 
+const updateStandard = async (useStandard: boolean) => {
+	const res = await fetch("/api/standard", {
+		method: "POST",
+		body: JSON.stringify({ useStandard }),
+	});
+	if (!res.ok) throw new Error("Error updating standard deck.");
+};
+
 const GuestDeck = ({ useStandard }: Props) => {
 	const router = useRouter();
-	const toggleStandard = async (useStandard: boolean) => {
-		await fetch("/api/standard", {
-			method: "POST",
-			body: JSON.stringify({ useStandard: !useStandard }),
-		});
-		router.refresh();
-	};
+	const [isSelected, setIsSelected] = useState(useStandard);
+
+	const updateStandardMutation = useMutation(updateStandard);
 
 	return (
 		<div className={`flex mt-2`}>
 			<div className="flex-1 item rounded-r-none border-r-0">
 				<p className="flex items-center">Standard Deck</p>
 			</div>
-			{useStandard ? (
+			{isSelected ? (
 				<div
 					className="btn rounded-l-none items-center flex"
-					onClick={() => toggleStandard(useStandard)}
+					onClick={() => {
+						setIsSelected(false);
+						updateStandardMutation.mutate(false, {
+							onSettled: () => router.refresh(),
+							onError: () => {
+								setIsSelected(true);
+							},
+						});
+					}}
 				>
 					<FaPause />
 				</div>
 			) : (
 				<div
 					className="btn rounded-l-none flex items-center"
-					onClick={() => toggleStandard(useStandard)}
+					onClick={() => {
+						setIsSelected(true);
+						updateStandardMutation.mutate(true, {
+							onSettled: () => router.refresh(),
+							onError: () => {
+								setIsSelected(false);
+							},
+						});
+					}}
 				>
 					<FaPlay />
 				</div>
