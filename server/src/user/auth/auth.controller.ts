73ec5a87adcffa3d51a796e829429d845c5a559f
filipslepcev <filipslepcev@yahoo.ca>
2022-11-17 19:42:59 -0500

    updated redirect

--- server/src/user/auth/auth.controller.ts
+++ server/src/user/auth/auth.controller.ts
@@ -30,12 +30,7 @@ export class AuthController {
 	}
 
 	@Get('/confirm/:token')
-	@Redirect(
-		process.env.FRONTEND_URL === 'http://localhost:8000'
-			? `${process.env.FRONTEND_URL}`
-			: `${process.env.FRONTEND_URL}/auth/confirm`,
-		301,
-	)
+	@Redirect(`${process.env.FRONTEND_URL}/auth/confirm`, 301)
 	emailVarification(@Param('token') token: string) {
 		return this.authService.verifyEmailConfirmation(token);
 	}
